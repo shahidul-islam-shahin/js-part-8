f3ffbdf3a3afbf34e346fed8006a64144157d274
shahidul-islam-shahin <shahinshahidulislam7@gmail.com>
2024-03-19 21:29:58 +0600

    js part 8

---
+++ script.js
@@ -0,0 +1,15 @@
+function name(firstName, lastName){
+    console.log(' hello ' + firstName + ' ' + lastName);
+}
+
+name('shahidul', 'islam shahin');
+
+function math(number, number2){
+    return number / number2;
+}
+
+console.log(math(8,12));
+
+let himorning = function gretting() {
+    
+}
